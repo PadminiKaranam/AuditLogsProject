571e784197426a83c4ca7457f94b53cfffe4292d
PadminiKaranam <paddukaranam2@gmail.com>
2026-08-22 11:59:24 +0530

    Initial commit: added audit-log-service project with ATTESTATION.md

---
+++ ATTESTATION.md
@@ -0,0 +1,5 @@
+Full Name: Padmini Karanam
+Email Address: paddukaranam2@gmail.com
+Assignment Title: audit-log-service
+Start Date: 22-08-2026
+End Date:
---
+++ src/main/java/com/persistent/AuditApplication.java
@@ -0,0 +1,13 @@
+package com.persistent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditApplication.class, args);
+	}
+
+}
